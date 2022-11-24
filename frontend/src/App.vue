7cc4f6e78590e3304b560254cf85a53e62f7a419
benonymity <benonymity@gmail.com>
2022-11-23 21:42:34 -0500

    update: the hilarity mounts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,7 @@
   <div className="friendactivity">
     <div ref="unfocus" className="friendactivity-content">
       <div className="friendactivity-content-top">Spotify Friend Activity</div>
+      <div classname="friendactivity-content-line"></div>
       <!-- <div className="friendactivity-content-input"> -->
       <!-- <input -->
       <!--   @keyup="trySearch()" -->
